5e000ce8fc2b959b6d5ef981b29c0ba19dad14db
Francisco Filho <francisco@capimgrosso.com>
2018-12-18 08:52:17 -0200

    add babel-class-properties plugin e formatacao Prettier

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import "whatwg-fetch";
 
-export default function App() {
-  return <div>
-    <h1>Hello World1</h1>
-  </div>
+export default class App extends React.Component {
+  state = {
+    id: null
+  };
+
+  mostrarAlerta() {
+    fetch("/javaee8/api/938", { method: "GET" })
+      .then(resp => resp.json())
+      .then(value => this.setState({ id: value.id }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <ul>
+          <li>
+            <a onClick={e => this.mostrarAlerta()}>Mostrar alerta com ID</a>
+          </li>
+        </ul>
+        <p>o id é {this.state.id}</p>
+      </div>
+    );
+  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,22 @@
-import React, { Component} from 'react'
-import ReactDOM from 'react-dom'
-import {AppContainer} from 'react-hot-loader'
-import App from './App'
-import './style.scss'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+import App from "./App";
+import "./style.scss";
 
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
       <Component />
     </AppContainer>,
-    document.getElementById('main')
-  )
-}
+    document.getElementById("main")
+  );
+};
 
-render(App)
+render(App);
 
 if (module.hot) {
-  module.hot.accept('./App', () => { render(App) })
+  module.hot.accept("./App", () => {
+    render(App);
+  });
 }
